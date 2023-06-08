fa98e159549253fc35a4e483cdf533371c38f6ae
FeeshSushi <j.k.h.lau1999@gmail.com>
2023-06-07 17:54:04 -0700

    Redux action and reducers in

--- down2meet/src/index.js
+++ down2meet/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducer);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
