e49b23b090820334a38c5d53dd46ed64d9675ea4
karatwan <karat.wannissorn@gmail.com>
2023-06-09 17:35:52 -0700

    integrated

--- down2meet/src/components/SocialGroups.js
+++ down2meet/src/components/SocialGroups.js
@@ -17,9 +17,9 @@ export default function SocialGroups() {
                 <button className="SGButton">Events</button> 
             </div>
       
-            <div className="CurrentEvents">
-                <i className="fa-solid fa-calendar-days"></i>
-                <button className="SGButton">Current Events</button> 
+            <div className="Groups">
+                <i class="fa-solid fa-people-group"></i>
+                <button className="SGButton">Groups</button>
             </div>
         </div>
     );
