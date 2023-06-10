740bf2681e48e6119f40bbed35ba382fd274f60d
karatwan <karat.wannissorn@gmail.com>
2023-06-09 21:58:59 -0700

    acquire name from user's google account
    can acquire name, google ID, profile picture URL, email
    
    >> Navigation.js

--- down2meet/src/components/Navigation.js
+++ down2meet/src/components/Navigation.js
@@ -2,17 +2,28 @@ import React from 'react';
 import logo from '../assets/D2MLogo.png';
 import { GoogleLogin } from '@react-oauth/google';
 import '../css/navigation.css';
+import { useState } from 'react';
 
 
 export default function Navigation() {
+
+    const [username, setUsername] = useState("");
+
     // when users successfully login
     const responseMessage = (response) => {
+      const idToken = response.credential;
+      const encodedPayload = idToken.split('.')[1];
+      const decodedPayload = JSON.parse(atob(encodedPayload));
+      setUsername(decodedPayload.name); //doesn't get updated until outside responseMessage, for rendering page after
+      const updated_name = decodedPayload.name;
+      console.log("Hey, " + updated_name);
       console.log(response);
     };
     // when users don't successfully login
     const errorMessage = (error) => {
       console.log(error);
     };
+
   return (
     <nav className="navbar">
       <a href="/" className="brand-logo">
@@ -25,7 +36,7 @@ export default function Navigation() {
         {/* using tutorial for GoogleLogin from: https://blog.logrocket.com/guide-adding-google-login-react-app/ */}
         <GoogleLogin onSuccess={responseMessage} onError={errorMessage} className="google-login-button" // Apply the CSS class here
 />
-
+      
 
       </div>
     </nav>
