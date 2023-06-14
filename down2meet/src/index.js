3b13b31397b0792ba840f4115ef396ca36b25845
FeeshSushi <j.k.h.lau1999@gmail.com>
2023-06-14 16:58:20 -0700

    Router implemented

--- down2meet/src/index.js
+++ down2meet/src/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './reducers/reducers';
+import reducer from './reducers/reducer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Friends from './pages/Friends';
+import Groups from './pages/Groups';
+import SocialGroups from './components/SocialGroups';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = createStore(reducer);
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  // using GoogleOAuthProvider tutorial from: https://blog.logrocket.com/guide-adding-google-login-react-app/
+ReactDOM.render(<>
+  <GoogleOAuthProvider clientId="1011482531322-6d1dp35f941hr37vnn7cvjdstntunnru.apps.googleusercontent.com">
+    <React.StrictMode>
+      <Router>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Friends" element={<Friends />} />
+          <Route path="/Groups" element={<Groups />} />
+          <Route path="/Events" element={<Events />} />
+        </Routes>
+      </Provider>
+      </Router>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+  </>,
+
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
