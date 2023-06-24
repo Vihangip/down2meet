dfed0bf0685d2b23e1a992f6ceb0a4bc68e65023
lucy li <luxi.lucy.li@gmail.com>
2023-06-23 21:34:14 -0400

    fixed friends

--- down2meet/src/components/ActiveUsers.js
+++ down2meet/src/components/ActiveUsers.js
@@ -1,29 +1,18 @@
 import React from 'react';
+import {userData} from '../tests/usersData'
 
 export default function ActiveUsers(){
-    const activeUsers = [
-        { id: 1, name: 'Jack Sparrow'},
-        { id: 3, name: 'Bob Builder'},
-        { id: 5, name: 'Dora Explorer'}
-    ]
-
-    const inactiveUsers = [
-        { id: 2, name: 'Tony Spark'},
-        { id: 4, name: 'Thanos Purpleguy'},
-        { id: 6, name: 'Groot'}
-    ]
 
     return (
         <div>
             <h2>Active Users</h2>
-            <ul className='activeUsers-list'>
-        {activeUsers.map(user => (
-          <li className="activeUser" key={user.id}>{user.name}</li>
-        ))}
-        {inactiveUsers.map(user => (
-          <li className="inactiveUser" key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+            <ul className="activeUsers-list">
+              {userData
+              .sort((a, b) => (a.availability === b.availability ? 0 : a.availability ? -1 : 1))
+              .map((user) => (
+              <li className={user.availability ? 'activeUser' : 'inactiveUser'} key={user.name}>{user.name}</li>
+      ))}
+  </ul>
     </div>
     );
 }
--- down2meet/src/pages/Friends.jsx
+++ down2meet/src/pages/Friends.jsx
@@ -1,5 +1,7 @@
-
-import React from 'react';
+import React, {useState} from 'react';
+import {userData} from '../tests/usersData';
+import Friend from '../components/Friend';
+import SortingComponent from '../components/Sorting';
 
 function Friends() {
   const [sortOrder, setSortOrder] = useState('default');
@@ -19,6 +21,24 @@ function Friends() {
       return a.availability === b.availability ? 0 : a.availability ? 1 : -1;
     });
   }
+
+  return (
+    <div className="FriendsPage">
+        <h1>Friends</h1>
+        <SortingComponent sortOrder={sortOrder} handleSortChange={handleSortChange} />
+        <div>
+      {sortedData.map((friend, index) => (
+        <Friend
+          key={index}
+          name={friend.name}
+          profilepic={friend.profilepic}
+          availability={friend.availability}
+        />
+      ))}
+    </div>
+    </div>
+  );
 }
 
 export default Friends;
+
