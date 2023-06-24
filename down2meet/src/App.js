37f0b5a12c62321f286be1941c09332c25195a0f
coluka <coluka@students.cs.ubc.ca>
2023-06-23 18:04:53 -0700

    Added Availablility Calendar
    Use the react-big-calendar framework.

--- down2meet/src/App.js
+++ down2meet/src/App.js
@@ -7,6 +7,7 @@ import SocialFeed from './components/SocialFeed';
 import ActiveUsers from './components/ActiveUsers';
 import PostBar from './components/PostBar';
 import SearchBar from './components/SearchBar';
+import Availability from './components/Availability';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <div className="Body-Middle">
           <PostBar />
           <SocialFeed />
+          <Availability/>
         </div>
         <div className="Body-Right">
           <ButtonAvailable />
--- down2meet/src/components/SocialFeed.js
+++ down2meet/src/components/SocialFeed.js
@@ -4,7 +4,6 @@ export default function SocialFeed() {
     return (
         <div className="SocialFeed">
             <div className="Post">
-                <h1> this is a post.</h1>
             </div>
         </div>
     );
