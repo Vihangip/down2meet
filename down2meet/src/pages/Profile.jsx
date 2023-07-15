badf0b68f1f83645cc4c1dca4e5e803786f9998b
karatwan <karat.wannissorn@gmail.com>
2023-07-14 17:24:20 -0700

    deleted unnecessary code, NOTE
    There's a problem with HTTP GET in retrieving the signed in user's picture

--- down2meet/src/pages/Profile.jsx
+++ down2meet/src/pages/Profile.jsx
@@ -10,32 +10,16 @@ import { getUsersAsync } from '../redux/user/thunks';
 
 function Profile() {
 
-  const defaultValue =  {
-    user_id: "johnsmith123@gmail.com",
-    name: "John Smith",
-    picture: "https://wallpapers.com/images/hd/basic-default-pfp-pxi77qv5o0zuz8j3.jpg"
-  };
-
-  const user = useSelector(state => state.reducer.user) || defaultValue;
-  //const friendsList = useSelector((state) => (state.users));
-  //dispatch(getOneUserAsync(email))
-
-  const dispatch = useDispatch();
-  const test_signin = useSelector(state => state.user);
-  useEffect(() => {
-    dispatch(getUsersAsync());
-    console.log(test_signin);
-  }, [dispatch]);
+  const user = useSelector(state => state.reducer.user);
 
 
-
-
+  //there's a problem with HTTP GET picture right now
   return (
     <div className="ProfilePage">
         <h2>Profile</h2>
         <div className="in-line">
           
-          <img className="ProfilePicture" src={user.picture} alt="profile"/>   
+          {/*<img className="ProfilePicture" src={user.picture} alt="profile"/>   */}
           <div className="column">
             <p> Name:   {user.name} </p> 
             <p> Email:   {user.user_id} </p>
