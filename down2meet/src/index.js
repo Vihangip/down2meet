e8f7945a2560a3022630ca0b0cb7f8f0fc561f3a
FeeshSushi <j.k.h.lau1999@gmail.com>
2023-07-29 09:19:34 -0700

    more small fix

--- down2meet/src/index.js
+++ down2meet/src/index.js
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Events from './pages/Events';
 import Friends from './pages/Friends';
 import Groups from './pages/Groups';
+import FriendProfile from './pages/FriendProfile';
 import Profile from './pages/Profile';
 import Notifications from './pages/Notification';
 import Hangouts from './pages/Hangouts';
