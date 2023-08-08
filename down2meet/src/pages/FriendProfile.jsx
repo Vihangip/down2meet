b5aa00475a58f2627c2c1094aa53271a31665f01
FeeshSushi <j.k.h.lau1999@gmail.com>
2023-08-08 16:49:29 -0700

    friendporilfe

--- down2meet/src/pages/FriendProfile.jsx
+++ down2meet/src/pages/FriendProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import BodyHeader from '../components/BodyHeader';
 import Navbar from '../components/Navbar';
 import ButtonAvailable from '../components/ButtonAvailable';
@@ -10,12 +10,33 @@ import { useLocation } from 'react-router-dom';
 
 function FriendProfile() {
   const location = useLocation();
+  const currentUser = JSON.parse(localStorage.getItem('user'));
   const friendInfo = location?.state?.friendInfo;
-
+  const [isApprovedFriend, setIsApprovedFriend] = useState(false);
   const dispatch = useDispatch();
-  useEffect (() => {
-    dispatch(getEventAsync(friendInfo.user_id));          
-  },[dispatch]);  
+
+  console.log(currentUser);
+  const fetchFriendProfile = async (userId) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_URL3001}/users/${userId}/approvedfriends`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch approved friends');
+      }
+      const approvedFriends = await response.json();
+      console.log("approve", approvedFriends);
+      console.log("current", currentUser.user_id )
+      setIsApprovedFriend(approvedFriends.includes(currentUser.user_id));
+    } catch (error) {
+      console.error(error);
+    }
+  };  
+
+  useEffect(() => {
+    if (friendInfo?.user_id) {
+      dispatch(getEventAsync(friendInfo.user_id));
+      fetchFriendProfile(friendInfo.user_id);
+    }
+  }, [friendInfo, dispatch]);
 
   return (
     <>
@@ -33,12 +54,17 @@ function FriendProfile() {
             </div>
           </div>
           <h3> Schedule</h3>
+          {isApprovedFriend ? (
             <div> <Event formLocation="profile" /> </div>
+          ) : (
+            <div> No availability to show </div>
+          )}
         </div>
       </div>
       <div className="Body-Right">
         <ButtonAvailable />
         <Search />
+        {/* <ActiveUsers /> */}
       </div>
     </>
   );
